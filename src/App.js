6cb1b25aa0fb750cfd28d6126019faa23c25b0a5
jingjin412 <b10204022@g.ntu.edu.tw>
2025-04-15 17:15:26 +0800

    all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Home from "./components/Home";
+import TicTacToe from "./components/TicTacToe";
+import GuessNumber from "./components/GuessNumber";
+import WhackAMole from "./components/WhackAMole";
+import TypingGame from "./components/TypingGame";
+import MathGame from "./components/MathGame";
 
 function App() {
+  const [currentGame, setGame] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentGame === "tic-tac-toe" && <TicTacToe setGame={setGame} />}
+      {currentGame === "guess-number" && <GuessNumber setGame={setGame} />}
+      {currentGame === "whack-a-mole" && <WhackAMole setGame={setGame} />}
+      {currentGame === "typing-game" && <TypingGame setGame={setGame} />}
+      {currentGame === "math-game" && <MathGame setGame={setGame} />}
+      {currentGame === null && <Home setGame={setGame} />}
     </div>
   );
 }
